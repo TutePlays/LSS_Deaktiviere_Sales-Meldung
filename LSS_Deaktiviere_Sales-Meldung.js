5f143966537e67111aeb68dcc6928aed089f4b48
TutePlays <tutedutsch@gmail.com>
2020-02-14 19:05:27 +0100

    Create LSS_Deaktiviere_Sales-Meldung.js

---
+++ LSS_Deaktiviere_Sales-Meldung.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         LSS Deaktiviere Sales-Meldung
+// @version      1
+// @description  Deaktiviert die grüne Hintergrundfarbe der Sales-Meldung
+// @author       TutePlays
+// @include      *://www.leitstellenspiel.de/
+// @include      *://www.leitstellenspiel.de/*
+// @include      *://www.missionchief.com/*
+// @include      *://www.missionchief.com/
+// @include      *://www.meldkamerspel.com/*
+// @include      *://www.meldkamerspel.com/
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    $("#coins_top").css("background-color", "inherit");
+})();
